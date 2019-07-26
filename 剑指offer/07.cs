ae65dcb0bb60e59c0240f86b07fbdeb4cee091bd
MrFangZ41 <mrfangz41@outlook.com>
2019-07-26 16:09:27 +0800

    重建二叉树

--- 剑指offer/07.cs
+++ 剑指offer/07.cs
@@ -8,3 +8,37 @@ public class TreeNode
         val=x;
     }
 }//create 
+
+public class Solution
+{
+    public TreeNode Rebuild(int[] preoder,int[] inoder)
+    {
+
+    }
+
+    public TreeNode Reconstruct(int[] preoder,int startpre,int endpre,int[] inoder,int startin,int endin)
+    {
+        if(startpre>endpre||startin>endin)
+        {
+            return null;
+        }
+
+        TreeNode root=new TreeNode(startpre);
+
+        for(int i=startin;i<=endin;i++)
+        {
+            if(inoder[i]==preoder[startpre])
+            {
+                root.left=Reconstruct(preoder,startpre+1,startpre+i-startin,inoder,startin,i-1);
+                root.right=Reconstruct(preoder,i-startin+startpre+1,endpre,inoder,i+1,endin);
+                break;
+
+                //startpre+1是前序遍历左子树开始的位置
+                //i-startin是中序遍历中左子树的长度
+                //startpre+(i-startin)是前序遍历左子树结束的位置
+            }
+
+            return root;
+        }
+    }
+}
