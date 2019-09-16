078b37351ee6104feb8423872d7d8fc0163e5129
MrFangZ41 <mrfangz41@outlook.com>
2019-09-16 16:20:51 +0800

    ads

---
+++ 剑指offer/47、礼物的最大价值.cs
@@ -0,0 +1,35 @@
+public class Solution
+{
+    public int GetMaxGift(int[,] matrix)
+    {
+        if(matrix==null||matrix.GetLength(0)<=0||matrix.GetLength(1)<=0)
+        {
+            return 0;
+        }
+
+        int[] dp=new dp[matrix.GetLength(1)];
+
+        for(int i=0;i<matrix.GetLength(0);i++)
+        {
+            for(int j=0;j<matrix.GetLength(1);j++)
+            {
+                int left=0;
+                int up=0;
+                
+                if(i>0)
+                {
+                    up=dp[j];
+                }
+
+                if(j>0)
+                {
+                    left=dp[j-1];
+                }
+
+                dp[j]=Math.Max(up,left)+matrix[i,j];
+            }
+        }
+
+        return dp[dp.Length-1];
+    }
+}
